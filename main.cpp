f9d0a04b5f42e5f5486ffa6ad021c9751538cf9a
zaidanahnaf <dhafaaza564@gmail.com>
2023-10-13 21:06:56 +0700

    Menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "---------------------------------------------------------------------------------------------------\n";
+    cout << "Nama: Dhafa Zaidan Ahnaf \n\n";
+    cout << "NPM: 2310631170073 \n\n";
+    cout << "Kelas: 1A-Informatika\n\n";
+    cout << "Cita-cita: Ingin menjadi seperti zucc dan menikah dengan gebetan saya \n\n";
+    cout << "Motivasi masuk Informatika: Penasaran dan tertarik untuk mengulik sistem \n\n";
+    cout << "yang berkerja dibalik sebuah aplikasi, software, dan berbagai macam teknologi yang terus berkembang\n";
+    cout << "---------------------------------------------------------------------------------------------------\n";
+
+    return 0;
+}
